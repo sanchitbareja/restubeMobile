946fc78f47913e84cf221bd21edbcf8e14db4d19
SpencerP <spencerpoff@gmail.com>
2013-06-19 13:23:36 -0400

    Send button fixed

--- app/view/Whiteboard.js
+++ app/view/Whiteboard.js
@@ -6,9 +6,30 @@ Ext.define('ResTube.view.Whiteboard', {
 	config: {
 		styleHtmlContent: true,
         scrollable: null,
-        tpl:"<div>To: {user.first_name}</div>"+
-        	"<div id='canvasDiv'></div>"+
-			'<textarea rows="4" cols="30" name="message" id="messageText"></textarea><br />',
+        tpl:"<div class='message_card up'>"+
+        		'<div class="row">'+
+					'<div class="small-12 columns">'+
+						"<b>From:</b> {from_user.first_name} {from_user.last_name}<br/>"+
+	        			"<b>To:</b> {to_user.first_name} {to_user.last_name}"+
+	        		'</div>'+
+				'</div>'+
+				'<div class="row">'+
+					'<div class="small-12 columns">'+
+						'<textarea rows="3" name="message" id="messageText"></textarea>'+
+					'</div>'+
+				'</div>'+
+				'<div class="row">'+
+					'<div class="small-12 columns">'+
+						"<div id='canvasDiv'></div>"+
+					'</div>'+
+				'</div>'+
+				'<div class="row">'+
+					'<div class="small-6 columns"></div>'+
+					'<div class="small-6 columns">'+
+						'<button type="button" value="Send" id="saveCanvasButton">Send</button>'+
+					'</div>'+
+				'</div>'+
+			"</div>",
 
 		items: [{
 			xtype: "toolbar",
@@ -17,24 +38,31 @@ Ext.define('ResTube.view.Whiteboard', {
 			items: [{
                 xtype: "button",
                 ui: "action",
-                text: "cancel",
+                text: "Cancel",
                 id: "cancelButton",
-            }, {
-            	xtype: 'spacer',
-            }, {
-            	xtype: 'button',
-            	ui: 'action',
-            	text: 'Send',
-            	id: 'saveCanvasButton',
             }]
-		}, {
+		},{
 	        itemId: 'browseFileBtn',
+	        id: 'browseFileBtn',
 	        xtype: 'fileupload',
 	        autoUpload: true,
 	        loadAsDataUrl: true,
+	        style: "color: #000;"+
+				  "font-size: 100%;"+
+				  "width: 45%;"+
+				  "vertical-align: top;"+
+				  "background-color: #fff;"+
+				  "padding: 0px;"+
+				  "margin-left: 5%;"+
+				  "border:1px solid #000;"+
+				  "border-radius: .3em;"+
+				  "-moz-border-radius: .3em;"+
+				  "-khtml-border-radius: .3em;"+
+				  "-webkit-border-radius: .3em;"+
+				  "top: 90.1%;",
 	        states: {
 	            browse: {
-	                text: 'Browse and load'
+	                text: 'Add Picture'
 	            },
 	            ready: {
 	                text: 'Load'
@@ -49,7 +77,8 @@ Ext.define('ResTube.view.Whiteboard', {
 	    }],
 
         listeners: [{
-        	delegate: "#saveCanvasButton",
+        	element: "element",
+        	delegate: "button#saveCanvasButton",
         	event: "tap",
         	fn: "onSaveCanvas",
         }, {
@@ -72,6 +101,7 @@ Ext.define('ResTube.view.Whiteboard', {
 			// do nothing
 		}
 		var to_user_data = this.getData();
+		console.log(to_user_data);
 
 		var root = this.element.dom;
 		var messageInput = root.querySelectorAll('textarea[name=message]')[0].value;
